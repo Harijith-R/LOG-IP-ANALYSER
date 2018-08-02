7d481e0278c685a659367cb65a5a21a37d001b92
Harijith-R <harijith321@gmail.com>
2018-08-02 23:40:45 +0530

    Final commit

--- ipcounter.py
+++ ipcounter.py
@@ -7,23 +7,23 @@ import collections
 import sys
 import os
 
-LogFile = "/home/harijith/Python/access.log"
+LogFile = "/home/harijith/Python/python-ip-counter/access.log"
 
 # This will exclude the Loopback IP Address both in IPV4 and IPV6
 EXCLUDES = ('127.0.0.1','::1')
 
+#Output Format:
+print('{:15}  : {}'.format("IP","COUNT"))
+
+# Initialise the IP Counter collections
 ipCounter = collections.Counter()
 
 with open(LogFile,'r') as fobj:
   for logLine in fobj:
-    ip = logLine.split()[0]
-    if ip not in EXCLUDES:
-      ipCounter.update((ip,))
+      ip = logLine.split()[0]
+      if ip not in EXCLUDES:
+          ipCounter.update((ip,))
       
-#print(ipCounter.most_common(5))
-#print()
-#print('-' * 25)
-#print(ipCounter.most_common(5)[::-1])
-
+# Print Output using Sting Formatting
 for ip,count in ipCounter.most_common(5):
   print('{:15}  : {}'.format(ip,count))
